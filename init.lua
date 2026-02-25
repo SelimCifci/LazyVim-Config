510540fd86ad6f8151441642327f568997ff32c2
SelimCifci <cifciselimhasan@gmail.com>
2026-02-25 16:03:50 +0100

    Initial commit

---
+++ init.lua
@@ -0,0 +1,8 @@
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
+
+local opt = vim.opt
+
+opt.tabstop = 4     -- Visual spaces per tab
+opt.softtabstop = 4 -- Spaces felt when editing
+opt.shiftwidth = 4  -- Size of an indent
